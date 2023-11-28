5bb664d279527e306d93a7338f74042ee5f5de5d
NONHIGH <angelones_24@hotmail.com>
2023-11-28 00:57:11 -0300

    login with styles

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginFormComponent } from './molecules/login-form/login-form.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LoginFormComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
--- src/app/pages/login/molecules/login-form/login-form.component.ts
+++ src/app/pages/login/molecules/login-form/login-form.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-login-form',
+  selector: 'form-login',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './login-form.component.html',
