f6e0f1e7947039b3d1f0a9e85d06266d67e1ebd5
NONHIGH <angelones_24@hotmail.com>
2023-11-27 22:15:41 -0300

    nonhigh: pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", redirectTo: "/home", pathMatch: 'full' },
+    { path: "home", component: DashboardComponent },
+    { path: "login", component: LoginComponent },
+    { path: "**", component: NotFoundComponent}
+];
